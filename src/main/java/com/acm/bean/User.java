b88d1a998f6fb46c3ba54035dbf222d9d22dd82b
longdsh <212977541@qq.com>
2017-11-08 15:53:49 +0800

    大概框架

---
+++ src/main/java/com/acm/bean/User.java
@@ -0,0 +1,9 @@
+package com.acm.bean;
+/**
+* @author 计算机网络应用 路素飞 E-mail:2512977541@qq.com
+* @version 创建时间：2017年11月8日 下午3:39:43
+* 类说明
+*/
+public class User {
+
+}
---
+++ src/test/resources/test.java
@@ -0,0 +1,8 @@
+/**
+* @author 计算机网络应用 路素飞 E-mail:2512977541@qq.com
+* @version 创建时间：2017年11月8日 下午3:38:13
+* 类说明
+*/
+public class test {
+
+}
